2221ce3ecea1212b53255a2671a0d291b4dbd459
MatheusKur <60073283+matheuskur7@users.noreply.github.com>
2024-06-13 21:22:33 -0300

    Exercícios da aula 05 concluídos com sucesso

---
+++ curso-js/modulo-02/aula-05/pilha-de-cartas/script.js
@@ -0,0 +1,24 @@
+let baralho = []
+
+let continua = true
+
+while(continua == true) {
+    let opcao = Number(window.prompt(`Baralho:\n${baralho} \n\nEscolha uma opção:\n[1] Adicionar carta\n[2] Retirar carta\n[3] Sair`))    
+    switch(opcao) {
+        case 1:
+            baralho.unshift(window.prompt("Qual o nome da carta? "))
+            break
+        case 2:
+            if(baralho.length > 0) {
+                window.alert('Carta removida: ' + baralho.shift())
+            } else {
+                window.alert("O baralho está vazio!")
+            }
+            break
+        case 3:
+            continua = false
+            break
+        default:
+            window.alert("Opção Iválida!!!")
+    }
+}
